dea1d856c261f0411b377e777b2fcf45030f04d2
Lukas Taake <lukas.taake@holisticon.de>
2026-05-20 10:26:50 +0200

    feat: add DefaultRole.ids()

--- api/src/main/java/io/camunda/security/api/model/authz/DefaultRole.java
+++ api/src/main/java/io/camunda/security/api/model/authz/DefaultRole.java
@@ -7,12 +7,18 @@
  */
 package io.camunda.security.api.model.authz;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum DefaultRole {
   ADMIN("admin"),
+  READONLY_ADMIN("readonly-admin"),
   RPA("rpa"),
   CONNECTORS("connectors"),
   APP_INTEGRATIONS("app-integrations"),
   TASK_WORKER("task-worker");
+
+  private static final List<String> IDS = Arrays.stream(values()).map(r -> r.id).toList();
 
   private final String id;
 
@@ -22,5 +28,9 @@ public enum DefaultRole {
 
   public String getId() {
     return id;
+  }
+
+  public static List<String> ids() {
+    return IDS;
   }
 }
